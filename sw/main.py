edaf793fff39fc8c6ba65c88dd153e427efb4ea5
kangasp <philip.kangas@gmail.com>
2024-12-07 23:50:43 -0800

    Update OTA.

--- sw/main.py
+++ sw/main.py
@@ -4,6 +4,7 @@ import urequests
 from time import sleep
 import machine
 import os
+import ota as ota
 
 SSID = ""
 PWORD = ""
@@ -25,33 +26,6 @@ def connect_wifi(ssid, password)->bool:
         sleep(0.5)
     return ret
 
-def latest_version_info(version_url):
-    print(f'Checking for latest version at: {version_url}')
-    response = urequests.get(version_url)
-    data = json.loads(response.text)
-    return data
-
-def save_from_url(file_name):
-    ret = False
-    file_url = URL + file_name
-    response = urequests.get(file_url)
-    if response.status_code == 200:
-        print(f'Fetched latest firmware code, status: {response.status_code}')
-        with open(file_name, 'w') as f:
-            f.write(response.text)
-        ret = True
-    else:
-        print(f'Firmware download error found - {file_url}, {response.status_code}.')
-    return ret
-
-def update_files(files):
-    ret = True
-    for f in files:
-        if not save_from_url(f):
-            print(f"FAILED update on file: {f}")
-            ret = False
-            break
-    return ret
 
 
 
