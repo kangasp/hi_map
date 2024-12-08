7643ab225b1e925884b0d65049ea7b9df58e4243
kangasp <philip.kangas@gmail.com>
2024-12-07 23:39:01 -0800

    Update main to test OTA.

--- sw/main.py
+++ sw/main.py
@@ -1,7 +1,14 @@
 import network
-
+import json
+import urequests
+from time import sleep
+import machine
 import os
 
+SSID = ""
+PWORD = ""
+URL = "https://raw.githubusercontent.com/kangasp/hi_map/main/sw/"
+V_URL = URL + "version.json"
 
 
 def connect_wifi(ssid, password)->bool:
@@ -18,14 +25,40 @@ def connect_wifi(ssid, password)->bool:
         sleep(0.5)
     return ret
 
-
-repo_url = "https://raw.githubusercontent.com/kangasp/hi_map/main/version.json"
-
-
-def latest_version(version_url):
+def latest_version_info(version_url):
     print(f'Checking for latest version at: {version_url}')
     response = urequests.get(version_url)
     data = json.loads(response.text)
-    print(f"data is: {data}, url is: {version_url}")
-    print(f"data version is: {data['version']}")
+    return data
+
+def save_from_url(file_name):
+    ret = False
+    file_url = URL + file_name
+    response = urequests.get(file_url)
+    if response.status_code == 200:
+        print(f'Fetched latest firmware code, status: {response.status_code}')
+        with open(file_name, 'w') as f:
+            f.write(response.text)
+        ret = True
+    else:
+        print(f'Firmware download error found - {file_url}, {response.status_code}.')
+    return ret
+
+def update_files(files):
+    ret = True
+    for f in files:
+        if not save_from_url(f):
+            print(f"FAILED update on file: {f}")
+            ret = False
+            break
+    return ret
+
+
+
+# connect_wifi(SSID, PWORD)
+# latest_version_info(V_URL)
+
+# response = urequests.get("https://raw.githubusercontent.com/kangasp/hi_map/main/sw/main.py")
+
+# machine.reset()
 
