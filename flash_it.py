a485836ff12bbb2adb57c2f78671c6edf0e85d24
Philip Kangas <kangasp@pax.local>
2025-08-06 17:51:07 -0700

    Moving main application code to app.

--- flash_it.py
+++ flash_it.py
@@ -2,15 +2,16 @@
 
 import subprocess
 import sys
-import argparse
 
-
-CORE="__init__.py colors.py fplot.py nanogui.py writer.py"
-WIDG="__init__.py  dial.py  label.py  led.py  meter.py  scale.py  textbox.py"
-FONT="arial10.py  arial35.py  arial_50.py  courier20.py  font10.py  font6.py  freesans20.py"
-EXTW="calendar.py  clock.py  eclock.py  grid.py"
-EXTD="calendar.py  clock_test.py  eclock_async.py  eclock_test.py"
-ROT="rotary.py  rotary_irq_esp.py"
+MP_CMD = "mpremote connect /dev/tty.usbserial* "
+DIRS = "drivers extras gui extras/widgets extras/demos gui/core gui/widgets gui/demos gui/fonts"
+CORE = "__init__.py colors.py fplot.py nanogui.py writer.py"
+WIDG = "__init__.py dial.py label.py led.py meter.py scale.py textbox.py"
+FONT = "arial10.py arial35.py arial_50.py courier20.py font10.py font6.py freesans20.py"
+EXTW = "calendar.py clock.py eclock.py grid.py"
+EXTD = "calendar.py clock_test.py eclock_async.py eclock_test.py"
+ROT = "rotary.py rotary_irq_esp.py"
+APP = "color_setup.py display.py epaper4in2.py my_app.py sleepy.py ota.py writer.py"
 
 ALL_FILES=[[ROT, "lib/micropython-rotary", "drivers"],
            [CORE, "lib/micropython-nano-gui/gui/core", "gui/core"],
@@ -19,41 +20,7 @@ ALL_FILES=[[ROT, "lib/micropython-rotary", "drivers"],
            [EXTW, "lib/micropython-nano-gui/extras/widgets", "extras/widgets"],
            [EXTD, "lib/micropython-nano-gui/extras/demos", "extras/demos"]]
 
-DIRS="drivers extras gui extras/widgets extras/demos gui/core gui/widgets gui/demos gui/fonts"
 
-# 
-# for i in $ROT; do
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-rotary/$i :
-# done
-# for i in $CORE; do
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/core/$i :/gui/core/
-# done
-# for i in $WIDG; do
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/widgets/$i :/gui/widgets/
-# done
-# for i in $FONT; do
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/fonts/$i :/gui/fonts/
-# done
-# for i in $EXTW; do
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/extras/widgets/$i :/extras/widgets/
-# done
-# for i in $EXTD; do
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/extras/demos/$i :/extras/demos/
-# done
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/hi_map/sw/color_setup.py :
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/hi_map/sw/epaper_42_v2.py :
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/drivers/boolpalette.py :drivers/
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/demos/aclock.py :gui/demos/
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/demos/epd_async.py :gui/demos/
-# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/extras/demos/clock_test.py :gui/demos/
-
-
-
-
-
-
-
-MP_CMD = "mpremote connect /dev/tty.usbserial* "
 
 def run_mpremote(args):
     result = subprocess.run(args, shell=True, capture_output=True, text=True)
--- install_to_board.sh
+++ install_to_board.sh
@@ -2,7 +2,6 @@
 #  Let's see if we can make the whole install, minus config files, work with this:
 #  $>  mpremote mip install "github:kangasp/hi_map"
 
-
 DEV=/dev/tty.usbserial-56230476401
 
 CORE="__init__.py colors.py fplot.py nanogui.py writer.py"
@@ -12,44 +11,58 @@ EXTW="calendar.py  clock.py  eclock.py  grid.py"
 EXTD="calendar.py  clock_test.py  eclock_async.py  eclock_test.py"
 ROT="rotary.py  rotary_irq_esp.py"
 
+CMD="mpremote connect /dev/tty.usbserial* "
+LS="$CMD fs --no-verbose ls"
+MKDIR="$CMD fs mkdir"
+RMDIR="$CMD rm -rf"
+
+$LS pk
+$LS
+
+cleanall
 
 
-CMD="mpremote connect id:5623047640 cp $SRC :$DST"
+DIRS="drivers extras extras/widgets extras/demos gui gui/core gui/widgets gui/demos gui/fonts"
+for D in $DIRS; do
+$RMDIR $D
+done
 
+$LS
+$LS
 
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir gui
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir drivers
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir extras
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir extras/widgets
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir extras/demos
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir  gui/core
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir  gui/widgets
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir  gui/demos
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir  gui/fonts
-
-for i in $ROT; do
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-rotary/$i :
-done
-for i in $CORE; do
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/core/$i :/gui/core/
-done
-for i in $WIDG; do
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/widgets/$i :/gui/widgets/
-done
-for i in $FONT; do
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/fonts/$i :/gui/fonts/
-done
-for i in $EXTW; do
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/extras/widgets/$i :/extras/widgets/
-done
-for i in $EXTD; do
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/extras/demos/$i :/extras/demos/
-done
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/hi_map/sw/color_setup.py :
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/hi_map/sw/epaper_42_v2.py :
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/drivers/boolpalette.py :drivers/
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/demos/aclock.py :gui/demos/
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/demos/epd_async.py :gui/demos/
-python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/extras/demos/clock_test.py :gui/demos/
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir gui
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir drivers
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir extras
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir extras/widgets
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir extras/demos
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir  gui/core
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir  gui/widgets
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir  gui/demos
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f mkdir  gui/fonts
+# 
+# for i in $ROT; do
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-rotary/$i :
+# done
+# for i in $CORE; do
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/core/$i :/gui/core/
+# done
+# for i in $WIDG; do
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/widgets/$i :/gui/widgets/
+# done
+# for i in $FONT; do
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/fonts/$i :/gui/fonts/
+# done
+# for i in $EXTW; do
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/extras/widgets/$i :/extras/widgets/
+# done
+# for i in $EXTD; do
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/extras/demos/$i :/extras/demos/
+# done
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/hi_map/sw/color_setup.py :
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/hi_map/sw/epaper_42_v2.py :
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/drivers/boolpalette.py :drivers/
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/demos/aclock.py :gui/demos/
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/gui/demos/epd_async.py :gui/demos/
+# python ~/git_things/micropython_bin/pyboard.py --device $DEV -f cp ~/git_things/micropython-nano-gui/extras/demos/clock_test.py :gui/demos/
 
 
